ec214bbf3a69c2d4405754a9f4d91f9e9bf074f6
David Bradley <d.bradley.1@warwick.ac.uk>
2019-10-15 12:29:44 +0100

    Ex.4: Create char, string letter, and output both

--- MPAGS-WEEK-1-CODE.cpp
+++ MPAGS-WEEK-1-CODE.cpp
@@ -3,8 +3,14 @@
 
 int main()
 {
+  const char* charlie{"c"};
+
   std::string msg {"Physics"};
- 
+
+  std::cout << charlie << std::endl;
+
+  std::cout << msg[5] << std::endl;
+  
   return 0;
 
 }
