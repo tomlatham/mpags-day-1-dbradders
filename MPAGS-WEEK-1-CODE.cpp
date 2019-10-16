9f2a9455d51ecd4349015581fe57fae3d99133db
David Bradley <d.bradley.1@warwick.ac.uk>
2019-10-16 12:31:23 +0100

    Ex. 5: Change numbers to words (runs but doesn't change numbers to words

--- MPAGS-WEEK-1-CODE.cpp
+++ MPAGS-WEEK-1-CODE.cpp
@@ -4,15 +4,48 @@
 int main()
 {
   char in_char{'x'};
-  char c;
+
+  std::string c {"y"};
+
   while (std::cin >> in_char)
     {
-      c = toupper(in_char);
-
+      switch (in_char)
+	{
+	    case 0:
+	       c = "ZERO";
+	       break;
+	    case 1:
+	       c = "ONE";
+	       break;
+	    case 2:
+	       c = "TWO";
+	       break;
+	    case 3:
+	       c = "THREE";
+	       break;
+	    case 4:
+	       c = "FOUR";
+	       break;
+	    case 5:
+	       c = "FIVE";
+	       break;
+ 	    case 6:
+	       c = "SIX";
+	       break;
+	    case 7:
+	       c = "SEVEN";
+	       break;
+	    case 8:
+	       c = "EIGHT";
+	       break;
+	    case 9:
+	       c = "NINE";
+	       break;
+	    default:
+               c = toupper(in_char);
+	}
       std::cout << c << std::endl;
     }
-
-
 
   return 0;
 }
